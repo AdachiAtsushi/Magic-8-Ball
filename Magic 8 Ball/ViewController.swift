6b3fff95dd8f5646f7759fe0d018fbc2f5dca565
Atsushi Adachi <atsuk0r05296@gmail.com>
2020-06-28 13:34:00 +0900

    Magic 8 Ball First Commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // ====== IBOutlet定義 ======
+    @IBOutlet weak var imageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    // ====== IBAction定義 ======
+    // 乱数(0~4)を生成後、画像が格納された配列より画像を取得
+    @IBAction func askButtonPressed(_ sender: Any) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+    
 }
 
